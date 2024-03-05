60927a88cfd2e4087cc826ab0bb7b262c55edc5b
Shreya388 <meenaxjha@gmail.com>
2024-03-05 22:49:24 +0530

    creating custom url shortener node.js and mongoDB

--- app.js
+++ app.js
@@ -13,12 +13,10 @@ connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1")
 .then(() => console.log('Mongodb connected!')
 );
 
-
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logReqRes('log.txt'));
-
 
 // Routes
 app.use("/api/users", userRouter);
