a11389f9f069825f84a631f302046b960554e1d0
Shreya388 <meenaxjha@gmail.com>
2024-03-08 20:28:17 +0530

    working with side rendering, EJS, Node.js

--- index.js
+++ index.js
@@ -11,9 +11,28 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
 
 app.use(express.json());
 
+app.get('/test', async (req, res) => {
+    allUrls = await URL.find({});
+    return res.end(`
+    <html>
+        <head>
+        </head>
+        <body>
+        <ol>
+        ${allUrls
+            .map(url => 
+            `<li>${url.shortId} = ${url.redirectURL} = ${url.visitHistory.length}</li>`
+            )
+            .join("")}
+        </ol>
+        </body>
+    </html>
+    `);
+});
+
 app.use("/url", urlRoute);
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
