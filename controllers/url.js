4e8a9a7773b635c701cdb78c9561bbca46b3cf56
Shreya388 <meenaxjha@gmail.com>
2024-03-13 21:50:44 +0530

    completed my work on EJS, Node.js server side rendering

--- controllers/url.js
+++ controllers/url.js
@@ -5,13 +5,16 @@ async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
     if(!body.url) return res.status(400).json({ error: 'url is required'})
     const shortID = shortid();
+    
     await URL.create({
         shortId: shortID,
         redirectURL: body.url,
         visitHistory: [],
     });
 
-    return res.json({ id: shortID });
+    return res.render("home", {
+        id: shortID,
+    });
 }
 
 module.exports = {
